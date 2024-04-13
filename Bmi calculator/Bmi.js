2f79524a0efc8638c713bf0ab1172f8f2f4f44c9
felixjack4260 <139462257+felixjack4260@users.noreply.github.com>
2024-04-13 15:03:40 +0530

    Create Bmi.js

---
+++ Bmi calculator/Bmi.js
@@ -0,0 +1,39 @@
+const btn1 = document.querySelector(".calculateBtn1");
+const btn2 = document.querySelector(".calculateBtn2");
+const fahrenheitBox = document.querySelector("#fahrenheit");
+const celsiusBox = document.querySelector("#celsius");
+
+
+btn1.addEventListener("click", () => {
+
+   
+    const celsius = Number.parseInt(celsiusBox.value);
+    let fahrenheit = 0;
+
+    
+    if (celsius === '' || isNaN(celsius) || typeof celsius === 'undefined' || !Number.isInteger(Number(celsius)))
+        celsiusBox.style.border = "2px solid red";
+    else 
+    {
+        celsiusBox.style.border = "2px solid gray";
+        fahrenheit = (celsius * 1.8) + 32;
+        fahrenheitBox.value = fahrenheit.toFixed(2) + "°F";
+    }
+});
+
+
+btn2.addEventListener("click", () => {
+
+    
+    const fahrenheit = Number.parseInt(fahrenheitBox.value);
+    let celsius = 0;
+
+    if (fahrenheit === '' || isNaN(fahrenheit) || typeof fahrenheit === 'undefined' || !Number.isInteger(Number(fahrenheit)))
+        fahrenheitBox.style.border = "2px solid red";
+    else 
+    {
+        fahrenheitBox.style.border = "2px solid gray";
+        celsius = (fahrenheit - 32) / 1.8;
+        celsiusBox.value = celsius.toFixed(2) + "°C";
+    }
+});
